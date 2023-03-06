812b2861e5e908738345e10d0b5216b40c0ae469
unknown <estiven.carmona@udea.edu.co>
2023-03-06 17:53:33 -0500

    Organización de typescript

---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "../Styles/NavBar";
+
+export default function NavBar() {
+    return (
+        <Container>
+            <nav>
+                <h1>Portfolio</h1>
+            </nav>
+        </Container>
+    )
+}
---
+++ src/Styles/NavBar.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.nav`
+    display: flex;
+    justify-content:space-evenly;
+    align-items: center;
+    color: white;
+    background-color: #131A22;
+`
