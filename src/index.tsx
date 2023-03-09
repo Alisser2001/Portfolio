76b35f0c2db788e8e4b0e52f2ea97ffe8dd17b4a
unknown <estiven.carmona@udea.edu.co>
2023-03-09 09:46:42 -0500

    Opciones de compilador arregladas

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (typeof document !== 'undefined') {
+  let element = document.getElementById('root') as HTMLElement;
+  const root = ReactDOM.createRoot(element);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
