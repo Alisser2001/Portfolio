39f56986c042baa62eac988b88bf67e8f4652a89
unknown <estiven.carmona@udea.edu.co>
2023-03-29 15:45:09 -0500

    Corrección al package

--- src/Components/Title.tsx
+++ src/Components/Title.tsx
@@ -3,13 +3,12 @@ import { HashLink as Link } from "react-router-hash-link";
 import { TitleContainer } from "../Styles/Title";
 
 export const Title: React.FC = ({}) => {
-    const width:number = window.innerWidth;
     return(
         <TitleContainer>
             <h1>PROFESIONAL</h1>
             <Link smooth to="/contact" className="anchor3">
                 <img src="/icons/correo-electronico.png"/>
-                {width<=900 && <p>Contact</p>}
+                {window.innerWidth<=900 && <p>Contact</p>}
             </Link>
         </TitleContainer>
     )
