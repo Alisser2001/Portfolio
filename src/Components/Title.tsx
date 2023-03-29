13381311592b9ef6db0b82b02771bba2cd096b3c
unknown <estiven.carmona@udea.edu.co>
2023-03-29 15:10:19 -0500

    Corrección al package

--- src/Components/Title.tsx
+++ src/Components/Title.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HashLink as Link } from "react-router-hash-link";
 import { TitleContainer } from "../Styles/Title";
 
 export const Title: React.FC = ({}) => {
-    const width = window.innerWidth;
+    let width:number = window.innerWidth;
+    useEffect(()=>{
+        width = window.innerWidth;
+    })
     return(
         <TitleContainer>
             <h1>PROFESIONAL</h1>
