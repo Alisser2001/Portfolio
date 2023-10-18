b45f31bf0f495201324b7f4685095411728a1938
unknown <estiven.carmona@udea.edu.co>
2023-10-18 09:26:24 -0500

    Pequeñas correciones: Diseño Navbar

--- app/page.jsx
+++ app/page.jsx
@@ -5,16 +5,11 @@ import Projects from './projects/page';
 import Contact from './contact/page';
 
 export default function App ({ }) {
-    const toTop = (e) => {
-        e.preventDefault();
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
     return (
         <div className="home">
             <AboutMe/>
             <Projects/>
             <Contact/>
-            <img src="rowTop.png" className="toTop" onClick={(e)=>toTop(e)}/>
         </div>
     );
 }
