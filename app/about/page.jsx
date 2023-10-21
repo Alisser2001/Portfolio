d8f291b7e3269f36992f1549af9795532018cc3b
unknown <estiven.carmona@udea.edu.co>
2023-10-20 22:26:43 -0500

    Corrección al idioma

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -17,18 +17,18 @@ export default function AboutMe({ }) {
                     : <div className={styles.CV} onMouseLeave={() => handleProfileView()}>
                         <img src="/CV.png" className={styles.profileCV} />
                         <div className={styles.links}>
-                            <a href="/CV.pdf" target="_blank">Ir al CV</a>
-                            <a href="https://drive.google.com/drive/folders/1BB-DEmcK4UqsKDXK4J4J1O0Q3CAUAsM5?usp=sharing" target="_blank">Certificados</a>
+                            <a href="/CV.pdf" target="_blank">Go to CV</a>
+                            <a href="https://drive.google.com/drive/folders/1BB-DEmcK4UqsKDXK4J4J1O0Q3CAUAsM5?usp=sharing" target="_blank">Certificates</a>
                         </div>
                     </div>}
             </div>
             <div className={styles.info}>
                 <p className={styles.presentation}>
-                    Soy Estiven Muñoz, desarrollador web full stack con experiencia trabajando
-                    en lenguajes como JavaScript, TypeScript, C#, Python y Java. Principalmente
-                    enfocado al desarrollo back-end, he participado en diferentes proyectos
-                    desarrollando funcionalidades CRUD para servidores web y en la creación de
-                    APIs RESTful.
+                I'm Estiven Muñoz, a full stack web developer with experience working in 
+                languages like JavaScript, TypeScript, C#, Python and Java. 
+                Mainly focused on back-end development, I've participated in different 
+                projects developing CRUD functionalities for web 
+                servers and creating RESTful APIs.
                 </p>
                 <ul className={styles.tecnologies} id="carousel-list">
                     <li><p>JavaScript</p></li>
