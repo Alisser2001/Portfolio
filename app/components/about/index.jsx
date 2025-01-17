ff5a0f36b735c1386a4b77fa7bfe676ffcccad3c
Alisser2001 <estiven.carmona@udea.edu.co>
2025-01-17 18:33:53 -0500

    Privera version del nuevo diseno

--- app/components/about/index.jsx
+++ app/components/about/index.jsx
@@ -4,7 +4,7 @@ export default function Index() {
     return (
         <div className={`flex flex-row w-full h-auto justify-end items-center mb-16`}>
             <div className='hidden md:flex h-1 w-1/3 md:w-3/5 bg-[#2E28A0]'></div>
-            <h1 className='font-bold text-3xl md:text-2xl lg:text-2xl xl:text-3xl w-full md:w-2/5 flex justify-center items-center'><span className='text-[#2E28A0] mr-5'>01.</span> About Me</h1>
+            <h1 className='font-bold text-2xl xl:text-3xl w-full md:w-2/5 flex justify-center items-center'><span className='text-[#2E28A0] mr-5'>01.</span> About Me</h1>
         </div>
     )
 }
