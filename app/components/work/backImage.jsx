d6be7cc61a76935a4e905f71cb2a34857dee7faa
Alisser2001 <estiven.carmona@udea.edu.co>
2025-02-01 11:05:51 -0500

    Breve modificacion del tamano de imagenes de proyectos

--- app/components/work/backImage.jsx
+++ app/components/work/backImage.jsx
@@ -3,7 +3,7 @@
 export default function BackImage({idx, obj}) {
     return (
         <div className={`w-full md:w-[90%] flex flex-row ${idx % 2 === 0 ? 'justify-start' : 'justify-end'} rounded-sm z-10 mr-0 ml-0`}>
-            <img src={obj.img} alt="project" className="rounded-md 2xl:scale-110 h-[250px] md:h-[350px] w-[90%] md:w-[550px]"/>
+            <img src={obj.img} alt="project" className="rounded-md 2xl:scale-110 h-[230px] md:h-[330px] w-[90%] md:w-[550px]"/>
         </div>
     )
 }
